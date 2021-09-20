daf7495742bac5a97772d10b502190772cd55a2f
Fajar Anggara <fanggara@tibco.com>
2021-09-20 16:40:38 +0700

    Separate metadata

--- extension/activity/maptree/activity.go
+++ extension/activity/maptree/activity.go
@@ -2,44 +2,10 @@ package maptree
 
 import (
 	"github.com/project-flogo/core/activity"
-	"github.com/project-flogo/core/data/coerce"
 	"github.com/project-flogo/core/support/log"
 )
 
-type Input struct {
-	SourceObject string `md:"source"`
-}
 
-func (r *Input) FromMap(values map[string]interface{}) error {
-	var err error
-	r.SourceObject, err = coerce.ToString(values["source"])
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r *Input) ToMap() map[string]interface{} {
-	return map[string]interface{}{
-		"source": r.SourceObject,
-	}
-}
-
-/*type Output struct {
-	AnOutput string `md:"output"`
-}
-
-func (o *Output) FromMap(values map[string]interface{}) error {
-	obj, _ := coerce.ToString(values["output"])
-	o.AnOutput = obj
-	return nil
-}
-
-func (o *Output) ToMap() map[string]interface{} {
-	return map[string]interface{}{
-		"output": o.AnOutput,
-	}
-}*/
 
 func init() {
 	_ = activity.Register(&Activity{},New) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
